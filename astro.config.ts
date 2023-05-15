4d6cc0f2337f7475c22c92edc610b29aafbbece8
Dev-Siri <s0431668@gmail.com>
2023-05-15 16:31:57 +0530

    Remove solid-js

--- astro.config.ts
+++ astro.config.ts
@@ -1,15 +1,12 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-// import solidJs from "@astrojs/solid-js";
 import image from "@astrojs/image";
 import compress from "astro-compress";
 import sitemap from "@astrojs/sitemap";
 
-// https://astro.build/config
 export default defineConfig({
   integrations: [
     tailwind(),
-    // solidJs(),
     compress(),
     sitemap(),
     image({
