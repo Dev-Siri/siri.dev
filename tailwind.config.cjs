6e8783287b3e4bf2b8b87197f3ac7d2375c5753d
Dev-Siri <s0431668@gmail.com>
2023-05-20 23:23:29 +0530

    Make the website somewhat responsive

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,6 +8,9 @@ module.exports = {
         "semi-dark-gray": "#1c1c1c",
         "semi-visible-blue": "rgba(0, 120, 255, 0.53)",
       },
+      fontSize: {
+        "31px": "31px!important",
+      },
     },
   },
   experimental: {
