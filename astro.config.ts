7560869c76fe9f8f0a5821508af5f8ffaa72d5d0
Dev-Siri <s0431668@gmail.com>
2023-05-29 17:49:57 +0530

    change to netlify

--- astro.config.ts
+++ astro.config.ts
@@ -15,5 +15,5 @@ export default defineConfig({
       serviceEntryPoint: "@astrojs/image/sharp",
     }),
   ],
-  site: "https://aarrush-dev.vercel.app",
+  site: "https://aarush-dev.netlify.app/",
 });
