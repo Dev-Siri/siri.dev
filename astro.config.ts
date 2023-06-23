c19c8bb87631ebea280f0d67e47269c638c15de7
Dev-Siri <s0431668@gmail.com>
2023-06-23 22:06:47 +0530

    Change build format from dir to file

--- astro.config.ts
+++ astro.config.ts
@@ -1,9 +1,9 @@
-import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
-import compress from "astro-compress";
-import sitemap from "@astrojs/sitemap";
 import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
+import compress from "astro-compress";
+import { defineConfig } from "astro/config";
 
 export default defineConfig({
   integrations: [
@@ -15,5 +15,8 @@ export default defineConfig({
       serviceEntryPoint: "@astrojs/image/sharp",
     }),
   ],
+  build: {
+    format: "file",
+  },
   site: "https://aarush-dev.netlify.app/",
 });
