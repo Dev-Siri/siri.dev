93066158af86389027a01a8a4bfbe2cf955acc44
Dev-Siri <s0431668@gmail.com>
2023-07-08 12:34:11 +0530

    Sharp failed compression recovery attempt #1

--- src/pages/api/image.ts
+++ src/pages/api/image.ts
@@ -10,17 +10,17 @@ export async function get({ url: { searchParams } }: APIContext) {
 
   const imageResponse = await fetch(imageUrl);
   const imageBuffer = await imageResponse.arrayBuffer();
+  const headers = new Headers();
 
   try {
-    const headers = new Headers();
     let optimizedImage: Buffer;
 
     if (imageResponse.headers.get("Content-Type")?.includes("image/gif")) {
       optimizedImage = await sharp(imageBuffer).resize(Number(height)).gif().toBuffer();
-      headers.append("Content-Type", "image/gif");
+      headers.set("Content-Type", "image/gif");
     } else {
       optimizedImage = await sharp(imageBuffer).resize(Number(height)).avif().toBuffer();
-      headers.append("Content-Type", "image/avif");
+      headers.set("Content-Type", "image/avif");
     }
 
     return new Response(optimizedImage, { headers });
@@ -28,6 +28,6 @@ export async function get({ url: { searchParams } }: APIContext) {
     console.log(error);
     // Return the original image as-is instead of silently erroring in the server console
     // So the user gets an actual (Although unoptimized) image
-    return new Response(imageBuffer);
+    return new Response(imageBuffer, { headers: imageResponse.headers });
   }
 }
