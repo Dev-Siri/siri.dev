cee4fe28b7c4e665448659ff853e9594065bd6bc
Dev-Siri <s0431668@gmail.com>
2023-09-01 13:37:27 +0530

    upgrade to Astro 3, remove astro-compress, & use built-in view-transitions

--- astro.config.ts
+++ astro.config.ts
@@ -1,16 +1,13 @@
-import { defineConfig } from "astro/config";
-
-// @ts-expect-error
 import netlify from "@astrojs/netlify/functions";
+import { defineConfig } from "astro/config";
 
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
-import compress from "astro-compress";
 import auth from "auth-astro";
 
 export default defineConfig({
-  integrations: [tailwind(), compress(), mdx(), sitemap(), auth()],
+  integrations: [tailwind(), mdx(), sitemap(), auth()],
   build: {
     format: "file",
   },
