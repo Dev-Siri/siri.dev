20952e8e9f2bd3f5e1c822215718c3a024899b13
Dev-Siri <s0431668@gmail.com>
2023-09-02 13:37:16 +0530

    add custom pages. netlify deployment fix #3

--- auth.config.ts
+++ auth.config.ts
@@ -11,4 +11,11 @@ export default {
       clientSecret: githubClientSecret,
     }),
   ],
+  pages: {
+    signIn: "/auth/signin",
+    signOut: "/auth/signout",
+    error: "/auth/error",
+    verifyRequest: "/auth/verify-request",
+    newUser: "/auth/new-user",
+  },
 } satisfies AuthConfig;
