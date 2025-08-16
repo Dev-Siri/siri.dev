d8daeb1ea4b646cf2b942dbc59a75fdfa8e6db5d
Dev-Siri <s0431668@gmail.com>
2025-08-16 23:00:15 +0530

    Change fonts and sizes and slight layouts of pages

--- src/islands/ProceedToCheckout.svelte
+++ src/islands/ProceedToCheckout.svelte
@@ -4,7 +4,7 @@
 
 <button
   type="button"
-  class="bg-white text-black p-2 px-4 rounded-full my-4 hover:bg-gray-200 duration-200"
+  class="cursor-pointer bg-white text-black p-2 px-4 rounded-full my-4 hover:bg-gray-200 duration-200"
   on:click={() => window.open(RICKROLL_URL)}
 >
   Proceed to Checkout
